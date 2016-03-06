4d9da28c81f4e870cf7eb43364646217cd8497e4
Tee Link <tesuanseylink@gmail.com>
2016-03-06 14:28:17 -0600

    correction

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+                Console.WriteLine(i);
+            {
+                
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz ");
+                }
+                 
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+             
+                Console.ReadLine();
+
+            }
+            
+        }
+    }
+}
