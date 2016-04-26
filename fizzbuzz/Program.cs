0d33788343bebf7268e65ed0243dcaec0dea13cf
Tee Link <tesuanseylink@gmail.com>
2016-04-25 22:54:05 -0500

    completed fizzbuzz

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -10,25 +10,32 @@ namespace fizzbuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 100; i++)
-                Console.WriteLine(i);
+            for (int i = 1; i <= 100; i++)
             {
-                
-                if (i % 3 == 0)
-                {
-                    Console.WriteLine("Fizz ");
-                }
-                 
-                if (i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
 
-             
-                Console.ReadLine();
+                //if ((i % 3 == 0) && (i % 5 == 0))
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    if (i % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz ");
+                    }
 
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
             }
-            
+            Console.ReadLine();
         }
     }
 }
